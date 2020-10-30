da5b7d787a0689082c39656cacc7a07da8e972a6
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2020-10-30 11:39:21 +0530

    word file loaded and integrated with the UI

--- alphabetScramble/ContentView.swift
+++ alphabetScramble/ContentView.swift
@@ -16,18 +16,22 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .autocapitalization(.none)
-                    .padding()
-                
-                List(usedWords, id: \.self) {
-                    Image(systemName: "\($0.count).circle")
-                    Text($0)
+            ZStack {
+                Color.gray
+                VStack {
+                    TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .padding()
+                    
+                    List(usedWords, id: \.self) {
+                        Image(systemName: "\($0.count).circle")
+                        Text($0)
+                    }
                 }
-            }
-        .navigationBarTitle(rootWord)
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+        }
         }
     }
     
@@ -41,11 +45,38 @@ struct ContentView: View {
         }
         
         //extra validation to come
-        
         usedWords.insert(answer, at: 0)
         newWord = ""
     }
-}
+    
+    // App initial data to start the game
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+        
+        // If were are *here* then there was a probleam - trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    // Typed word validation section
+    
+    
+    
+    
+    
+}// end of content view
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
