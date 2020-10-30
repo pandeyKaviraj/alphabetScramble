8cb32542f9acb5ef13063cd822b3360ea7100f99
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2020-10-30 11:07:04 +0530

    word file added to the project

--- alphabetScramble/ContentView.swift
+++ alphabetScramble/ContentView.swift
@@ -10,14 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        let word = "swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+        .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        //lowercase and trim the word, to make sure we dont add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        return Text("Hello World")
+        //exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+        
+        //extra validation to come
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
